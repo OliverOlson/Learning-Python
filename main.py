44597667850443e3b0f1e9ca79103b234a04d2f5
OliverOlson <oliverfrank2011@gmail.com>
2026-04-10 17:46:00 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_choices(): #def = define, get_choices is a function
+    player_choice = "rock" # player_choice is  a variable
+    computer_choice = "paper" # "paper" is a string
+
+    return computer_choice
+
+def greeting():
+    return "Hi"
+
+response = greeting()
+print(response)
